8b46670a0e6d5af7c281ca11a5a55b7472342d10
Jul Jen <juliajen.prog@gmail.com>
2018-04-28 21:36:56 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -32,6 +32,16 @@ class SongsController < ApplicationController
     erb :'/songs/edit'
   end
 
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+
+    flash[:message] = "Success! Song updated!"
+    redirect "/songs/#{@song.slug}"
+  end
+
 
   post '/set-flash' do
     # Set a flash entry
