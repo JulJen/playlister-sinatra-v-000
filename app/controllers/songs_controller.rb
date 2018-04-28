78c3a06c8b8638ec0f5ed8b38d07e05465eff425
Jul Jen <juliajen.prog@gmail.com>
2018-04-28 22:24:24 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,6 +17,12 @@ class SongsController < ApplicationController
     erb :'/songs/show'
   end
 
+
+  get "/songs/#{@song.slug}" do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
+  
   post '/songs' do
     @song = Song.create(:name => params["song_name"])
     @song.artist = Artist.find_or_create_by(:name => params["artist_name"])
