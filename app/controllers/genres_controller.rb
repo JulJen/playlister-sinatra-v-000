b5869e2071c0b28b21f962eb8694693bba171a46
Jul Jen <juliajen.prog@gmail.com>
2018-04-28 20:30:36 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,7 +1,13 @@
 class GenresController < ApplicationController
 
-  get '/' do
-    erb :index
+  get '/genres' do
+    @genres = Genre.all
+    erb :'genres/index'
+  end
+
+  get '/artists/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'genre/show'
   end
 
 end
