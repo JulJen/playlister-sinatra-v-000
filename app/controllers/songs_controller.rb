8029194ca4a65d5b7f787057f167a107b85ed5a6
Jul Jen <juliajen.prog@gmail.com>
2018-04-29 01:36:01 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,15 +5,18 @@ class SongsController < ApplicationController
 
   get '/songs' do
     @songs = Song.all
+    
     erb :'songs/index'
   end
 
   get '/songs/new' do
+    
     erb :'songs/new'
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
+    
     erb :'/songs/show'
   end
 
@@ -29,7 +32,7 @@ class SongsController < ApplicationController
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-    # @genres = Genre.all
+  
     erb :'/songs/edit'
   end
 
